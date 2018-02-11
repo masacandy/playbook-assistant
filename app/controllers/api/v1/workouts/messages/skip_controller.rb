29f3838bd33dbbdda2a394e67cd89d00fe41f9dd
masacandy <masajames.1016@gmail.com>
2018-02-11 22:57:06 +0900

    Revert "スキップする時にログがなければまた選べるようにする"
    This reverts commit abc6deae1c7e212755717548fd920e5dd8088284.

--- app/controllers/api/v1/workouts/messages/skip_controller.rb
+++ app/controllers/api/v1/workouts/messages/skip_controller.rb
@@ -11,12 +11,16 @@ class Api::V1::Workouts::Messages::SkipController < Api::V1::BaseController
     last_exercise_log = @workout&.user_exercise_logs&.where(exercise_id: exercise.id)&.last
 
     ActiveRecord::Base.transaction do
-      # 1度でもその種目をやっていたら終わりにする。1度もログなかったらそのままスキップする
-      create_user_last_exercise_log(exercise_id: exercise.id, weight: last_exercise_log.weight) if last_exercise_log.present?
+      if last_exercise_log.present?
+        create_user_last_exercise_log(exercise_id: exercise.id, weight: last_exercise_log.weight)
+      else
+        create_empty_exercise_log(exercise.id)
+        create_user_last_exercise_log(exercise_id: exercise.id, weight: ZERO_WEIGHT)
+      end
 
       WorkoutMessage.create!(
         workout_id: @workout.id,
-        message: "#{exercise.name}をスキップします",
+        message: "#{exercise.name}を終了します",
         message_type: WorkoutMessage.message_types[:user],
         next_action_type: WorkoutMessage.next_action_types[:assistant_message],
       )
