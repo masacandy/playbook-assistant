a6fbd3178a9f0f4f897b1105aa4c9fe39ad1c4c3
masacandy <masajames.1016@gmail.com>
2018-03-19 22:15:12 +0900

    0回でスキップした場合はもう一度選べるようにする

--- app/controllers/api/v1/workouts/messages/skip_controller.rb
+++ app/controllers/api/v1/workouts/messages/skip_controller.rb
@@ -13,17 +13,20 @@ class Api::V1::Workouts::Messages::SkipController < Api::V1::BaseController
     ActiveRecord::Base.transaction do
       if last_exercise_log.present?
         create_user_last_exercise_log(exercise_id: exercise.id, weight: last_exercise_log.weight)
+        WorkoutMessage.create!(
+          workout_id: @workout.id,
+          message: "#{exercise.name}を終了します",
+          message_type: WorkoutMessage.message_types[:user],
+          next_action_type: WorkoutMessage.next_action_types[:assistant_message],
+        )
       else
-        create_empty_exercise_log(exercise.id)
-        create_user_last_exercise_log(exercise_id: exercise.id, weight: ZERO_WEIGHT)
+        WorkoutMessage.create!(
+          workout_id: @workout.id,
+          message: "#{exercise.name}をスキップします",
+          message_type: WorkoutMessage.message_types[:user],
+          next_action_type: WorkoutMessage.next_action_types[:assistant_message],
+        )
       end
-
-      WorkoutMessage.create!(
-        workout_id: @workout.id,
-        message: "#{exercise.name}を終了します",
-        message_type: WorkoutMessage.message_types[:user],
-        next_action_type: WorkoutMessage.next_action_types[:assistant_message],
-      )
 
       ::CreateRecommendedWorkoutExerciseMessageService.call(workout_id: @workout.id)
     end
@@ -44,16 +47,6 @@ class Api::V1::Workouts::Messages::SkipController < Api::V1::BaseController
       exercise_id: exercise_id,
       weight: weight,
       weight_up: false,
-    )
-  end
-
-  def create_empty_exercise_log(exercise_id)
-    UserExerciseLog.create!(
-      user_id: current_user.id,
-      workout_id: @workout.id,
-      exercise_id: exercise_id,
-      weight: ZERO_WEIGHT,
-      reps: ZERO_REPS,
     )
   end
 
